fa4472f04a5b02aa777c13c40cada26461633974
thomaslrt05 <thomaslaurent771@gmail.com>
2024-04-17 09:59:57 +0200

    #25

--- src/conference-ia/src/Components/TextToSpeech.tsx
+++ src/conference-ia/src/Components/TextToSpeech.tsx
@@ -13,7 +13,7 @@ const TextToSpeech: React.FC<ConferenceContext> = ({ content }) => {
       
       const key : string = import.meta.env.VITE_COG_SVC_SUB_KEY;
       console.log(key);
-      let speechConfig = sdk.SpeechConfig.fromSubscription(key, "westeurope");
+      let speechConfig = sdk.SpeechConfig.fromSubscription(key, import.meta.env.VITE_COG_SVC_REGION);
       speechConfig.speechSynthesisVoiceName = "fr-FR-CoralieNeural";
       let synthesizer = new sdk.SpeechSynthesizer(speechConfig);
 

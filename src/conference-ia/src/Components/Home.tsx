8a88ca1b84f870393b14257a7bb3665c5aee127d
thomaslrt05 <thomaslaurent771@gmail.com>
2024-04-19 10:50:43 +0200

    #21

--- src/conference-ia/src/Components/Home.tsx
+++ src/conference-ia/src/Components/Home.tsx
@@ -2,30 +2,24 @@ import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold underline text-titre mb-10">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-10">
+      <h1 className="text-4xl font-bold text-blue-600">
         Page d'accueil
       </h1>
-      <nav>
-        <ul className="space-y-10 ">
-          <li>
-            <Link 
-              to="/CreateConference" 
-              className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
-            >
-              Création d'une conférence
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/Conference" 
-              className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600"
-            >
-              Accès à la conférence
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="flex space-x-4">
+        <Link 
+          to="/CreateConference" 
+          className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-200"
+        >
+          Création d'une conférence
+        </Link>
+        <Link 
+          to="/Conference" 
+          className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition-colors duration-200"
+        >
+          Accès à la conférence
+        </Link>
+      </div>
     </div>
   );
 };

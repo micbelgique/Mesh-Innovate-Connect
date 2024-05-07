f7b9766f2550dc401f49053414476a72fbd99c7a
Oscar <75579209+MissonO@users.noreply.github.com>
2024-05-07 15:03:39 +0200

    sauvegarde après suppression fichiers

--- src/MIC/Assets/.MeshCloudScripting/MIC-Maquette/obj/Debug/net6.0/MIC-Maquette.AssemblyInfo.cs
+++ src/MIC/Assets/.MeshCloudScripting/MIC-Maquette/obj/Debug/net6.0/MIC-Maquette.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIC-Maquette")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1ca5be2cef09fd71d997ae46ae7e8f158311ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46dce6f371e6084b18455d4b5267cee70d5ac0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIC-Maquette")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIC-Maquette")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
